52af9c40059b96dbe04172086c05bb284677fbde
Ripty-Dx <riptyfullstack.dx@gmail.com>
2024-01-17 10:49:18 +0530

    sales report date filters

--- src/api/useSalesReport.js
+++ src/api/useSalesReport.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-const useSalesReport = (props) => {
-  // console.log(JSON.parse(localStorage.getItem("Api Data")));
+const useSalesReport = (startDate,endDate) => {
   return {
     salesReportData: async (props) => {
       const response = await axios.post(
         "https://dev.beautyfashionsales.com/9kJs2I6Bn/i0IT68Q8&0",
         {
           salesRepId: JSON.parse(localStorage.getItem("Api Data")).data.Sales_Rep__c,
+          startDate:startDate,
+          endDate:endDate
         }
       );
       // const response = await axios.post("https://dev.beautyfashionsales.com/report/4i1cKeDt9");
