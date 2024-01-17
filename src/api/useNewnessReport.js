8eccaf18f83fe3f550ff28980997a5f1099325c3
Ripty-Dx <riptyfullstack.dx@gmail.com>
2024-01-17 18:59:50 +0530

    apply button added in newness report

--- src/api/useNewnessReport.js
+++ src/api/useNewnessReport.js
@@ -1,27 +1,45 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 export const useNewnessReport = (props) => {
   // console.log("props", props);
   const [data, setData] = useState();
-  useEffect(() => {
-    fetchNewnessApiData(props.ManufacturerId__c, props.fromDate, props.toDate);
-  }, [props]);
+  // useEffect(() => {
+  //   fetchNewnessApiData(props.ManufacturerId__c, props.fromDate, props.toDate);
+  // }, [props]);
 
-  const fetchNewnessApiData = (ManufacturerId__c, fromDate, toDate) => {
-    fetch("https://dev.beautyfashionsales.com/newness", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-        Brand: ManufacturerId__c,
-        toDate: toDate,
-        fromDate: fromDate,
-      },
-    })
-      .then((response) => response.json())
-      .then((result) => setData(result))
-      .catch((err) => console.log(err));
+  // const fetchNewnessApiData = (ManufacturerId__c, fromDate, toDate) => {
+  //   fetch("https://dev.beautyfashionsales.com/newness", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-type": "application/json",
+  //       Brand: ManufacturerId__c,
+  //       toDate: toDate,
+  //       fromDate: fromDate,
+  //     },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((result) => setData(result))
+  //     .catch((err) => console.log(err));
+  // };
+
+  // //   console.log(data);
+  // return data;
+  return {
+    fetchNewnessApiData: async ({ManufacturerId__c, fromDate, toDate}) => {
+      const response = await axios.post(
+        "https://dev.beautyfashionsales.com/newness",
+        {},
+        {
+          headers: {
+            "Content-type": "application/json",
+            Brand: ManufacturerId__c,
+            toDate: toDate,
+            fromDate: fromDate,
+          },
+        }
+      );
+      return response.data;
+    },
   };
-
-  //   console.log(data);
-  return data;
 };
