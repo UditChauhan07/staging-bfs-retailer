ef5ac7fae8ee5462f0f1bd5382a27a045808ec07
Ripty-Dx <riptyfullstack.dx@gmail.com>
2024-01-24 14:30:44 +0530

    Lead form validation and api integration

---
+++ src/api/usePublicManufacturers.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const access_token = async () => {
+  const response = await axios.post("https://dev.beautyfashionsales.com/beauty/login");
+  return response.data.data.access_token;
+};
+export const usePublicManufacturers = () => {
+  return {
+    manufacturers: async () => {
+      const key = await access_token();
+      let body = {
+        key: String(key),
+        link: "https://beautyfashionsales.my.salesforce.com/services/data/v56.0/query?q=SELECT Id, Name, Manufacturer_Logo__c,IsActive__c FROM Manufacturer__c where IsActive__c= 'active'  order by name",
+      };
+      const response = await axios.post("https://dev.beautyfashionsales.com/beauty/B0F9FC7237C", body);
+      return response.data.data.records;
+    },
+  };
+};
