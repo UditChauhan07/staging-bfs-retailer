4e466c87db2c00dd52092ee2b27e94e665a05591
sangeetadx <sangeetadhiman.dx@gmail.com>
2024-07-26 17:48:43 +0530

    25.07.24.03

--- src/validation schema/AccountInfoValidation.jsx
+++ src/validation schema/AccountInfoValidation.jsx
@@ -1,5 +1,12 @@
 import * as Yup from "yup";
 
 export const AccountInfoValidation = Yup.object().shape({
+    brand:Yup.object().required("Please select Brand name."),
+    description :Yup.string().required("Please enter description.").min(1, "Use 1 characters or more for description."),  
+})
+
+export const UpdateInfoWithStoreValidation = Yup.object().shape({
+    account:Yup.object().required("Please select Store name."),
+    brand:Yup.object().required("Please select Brand name."),
     description :Yup.string().required("Please enter description.").min(1, "Use 1 characters or more for description."),  
 })
