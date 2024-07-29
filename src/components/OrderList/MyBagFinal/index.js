24929695704651db401aa635ac9b6bc15b06c955
sangeetadx <sangeetadhiman.dx@gmail.com>
2024-07-29 14:19:07 +0530

    29.07.24.07

--- src/components/OrderList/MyBagFinal/index.js
+++ src/components/OrderList/MyBagFinal/index.js
@@ -195,12 +195,14 @@ function MyBagFinal({ setOrderDetail, generateXLSX, generatePdfServerSide }) {
     }
   }
 
-  const ReasonCardHandler = ({ reasonType, label = null }) => {
+  const ReasonCardHandler = ({ reasonType, label = null,haveValue=false }) => {
     if (reasonType) {
       if (!oldSupport?.[helpId]?.[reasonType]?.Id) {
-        return (<p onClick={() => { supportHandler(reasonType) }} style={reason == reasonType ? { color: '#0d6efd' } : {}}>&bull;&nbsp;{label ?? reasonType}</p>)
+        if(!haveValue){
+          return (<p onClick={() => { supportHandler(reasonType) }} style={reason == reasonType ? { color: '#0d6efd' } : {}}>&bull;&nbsp;{label ?? reasonType}</p>)
+        }
       } else {
-        return (<p onClick={() => setRestrict(reasonType)} style={reason == reasonType ? { color: '#0d6efd' } : {}}>&bull;&nbsp;{label ?? reasonType}<SupportTransporter Type={helpId} Reason={reasonType} /></p>)
+          return (<p onClick={() => setRestrict(reasonType)} style={reason == reasonType ? { color: '#0d6efd' } : {}}>&bull;&nbsp;{label ?? reasonType}<SupportTransporter Type={helpId} Reason={reasonType} /></p>)
       }
     } else {
       return null;
@@ -468,9 +470,9 @@ function MyBagFinal({ setOrderDetail, generateXLSX, generatePdfServerSide }) {
                         </div>
                         {helpId == "0123b0000007zc8AAA" &&
                           <div className={Styles.SupportHolder}>
-                            <ReasonCardHandler label={"Request Status Updates"} reasonType={"Status of Order"} />
-                            <ReasonCardHandler label={"Request Invoice"} reasonType="Invoice" />
-                            <ReasonCardHandler label={"Request Tracking number"} reasonType="Tracking Status" />
+                            <ReasonCardHandler label={"Request Status Updates"} reasonType={"Status of Order"} haveValue={OrderData.Order_Number__c}/>
+                            <ReasonCardHandler label={"Request Invoice"} reasonType="Invoice" haveValue={invoices?.length != 0}/>
+                            <ReasonCardHandler label={"Request Tracking number"} reasonType="Tracking Status" haveValue={OrderData.Tracking__c}/>
                           </div>}
 
                       </div>
--- src/pages/EducationCenter.jsx
+++ src/pages/EducationCenter.jsx
@@ -10,12 +10,10 @@ import AppLayout from "../components/AppLayout";
 const EducationCenter = () => {
   return (
     <AppLayout>
-      <div className="row d-flex flex-column justify-content-around align-items-center lg:min-h-[300px] xl:min-h-[400px]">
-        <div className="col-4">
-          <p className="m-0 fs-2 font-[Montserrat-400] text-[14px] tracking-[2.20px]">
+      <div className="row d-flex flex-column justify-content-center align-items-center lg:min-h-[300px] xl:min-h-[400px]">
+          <p className="m-0 fs-2 font-[Montserrat-400] text-center text-[14px] tracking-[2.20px]">
             Coming Soon...
           </p>
-        </div>
       </div>
       {/* <OrderStatusFormSection /> */}
     </AppLayout>
