7f6e4dcee1806dc3984c3b87050db39bc9c9aef2
Manisharanidx <manisharani.dx@gmail.com>
2024-07-28 21:57:32 -0700

    Retailer HelpSection

--- src/pages/HelpSection.jsx
+++ src/pages/HelpSection.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+
+import React, { useState, useRef, useEffect } from "react";
 import styles from "./page.module.css";
 import AppLayout from "../components/AppLayout";
 import { productGuides } from "../lib/store";
@@ -8,18 +9,23 @@ import { MdOutlineDownload } from "react-icons/md";
 import ReactPlayer from 'react-player';
 import FilterSearch from "../components/FilterSearch";
 import Loading from "../components/Loading";
+import { MdSlideshow } from "react-icons/md";
 const HelpSection = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentLink, setCurrentLink] = useState('');
   const [currentType, setCurrentType] = useState('');
+  const [currentFileName, setCurrentFileName] = useState('');
   const [isDownloadConfirmOpen, setIsDownloadConfirmOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
 
+  const modalRef = useRef(null);
+
   const guides = Object.values(productGuides);
 
-  const openModal = (link, type) => {
+  const openModal = (link, type, fileName) => {
     setCurrentLink(link);
     setCurrentType(type);
+    setCurrentFileName(fileName);
     setIsModalOpen(true);
   };
 
@@ -27,6 +33,7 @@ const HelpSection = () => {
     setIsModalOpen(false);
     setCurrentLink('');
     setCurrentType('');
+    setCurrentFileName('');
   };
 
   const openDownloadConfirm = () => {
@@ -58,29 +65,35 @@ const HelpSection = () => {
     guide.filename.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        closeModal();
+      }
+    };
+
+    if (isModalOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isModalOpen]);
+
   return (
     <AppLayout
-    filterNodes={
+      filterNodes={
         <>
-         <FilterSearch
+          <FilterSearch
             onChange={(e) => setSearchTerm(e.target.value)}
             value={searchTerm}
             placeholder={"Search by Name"}
             minWidth={"130px"}
             className={styles.searchInput}
           />
-         
-          {/* <button
-            className="border px-2 py-1 leading-tight d-grid"
-            onClick={() => {
-              // setHighestRetailers(true);
-              setSearchBy("");
-              setSortBy("");
-            }}
-          >
-            <CloseButton crossFill={'#fff'} height={20} width={20} />
-            <small style={{ fontSize: '6px',letterSpacing: '0.5px',textTransform:'uppercase'}}>clear</small>
-          </button> */}
         </>
       }
     >
@@ -88,7 +101,7 @@ const HelpSection = () => {
         <ModalPage
           open
           content={
-            <div className="d-flex flex-column gap-3" style={{ width: '75vw' }}>
+            <div ref={modalRef} className="d-flex flex-column gap-3" style={{ width: '75vw' }}>
               <div style={{
                 position: 'sticky',
                 top: '0',
@@ -97,46 +110,49 @@ const HelpSection = () => {
                 padding: '15px',
                 borderBottom: '1px solid #ddd',
               }}>
-                <div className="d-flex align-items-center justify-content-between" style={{ minWidth: '75vw' }}>
-                  <h1 className="font-[Montserrat-500] text-[22px] tracking-[2.20px] m-0 p-0">
-                    How to Access BFSG Retailer Portal {currentType}
+                <div className="d-flex align-items-center justify-content-between " style={{ minWidth: '75vw', marginTop:"-30px",marginLeft:'-20px' }}>
+                <div className="d-flex justify-content-end mt-2 gap-3">
+                  <h1 className="font-[Montserrat-500] text-[22px] tracking-[2.20px] m-0 p-0" style={{ fontSize:'18px' }}>
+                  {currentFileName} 
                   </h1>
-                  <div className="d-flex justify-content-end mt-2">
+                 
                     <button
                       className={styles.downloadButton}
                       onClick={openDownloadConfirm}
                     >
-                      <MdOutlineDownload size={20} />
-                      <small style={{ fontSize: '10px', letterSpacing: '0.5px', marginLeft: "-15px", textTransform: 'uppercase' }}>Download</small>
+                       <div className="d-flex align-items-center justify-content-between gap-1" >
+                       <MdOutlineDownload size={16} />Download
+                       </div>
                     </button>
                   </div>
-                  <button type="button" onClick={closeModal} >
+
+                  <button type="button" onClick={closeModal} style={{marginLeft:"50px"}} >
                     <CloseButton />
                   </button>
                 </div>
-                <hr />
               </div>
               {currentType === 'Video' ? (
                 <ReactPlayer
                   url={currentLink}
-                  width="100%"
+                  width="104%"
                   height="400px"
                   overflow="hidden"
+                  style={{marginLeft:"-20px"}}
+              
                   controls
-                />
+                  ></ReactPlayer>
               ) : (
                 <iframe
                   src={currentLink}
-                  width="100%" height="400px" overflow="hidden"></iframe>
+                 style={{ width:"104%", height:"400px" ,marginLeft:"-20px",  overflow:"hidden"}}></iframe>
               )}
               {isDownloadConfirmOpen &&
                 <div className={styles.modalOverlay}>
                   <div className={styles.modalContent}>
-                    <h2>Download {currentType.toLowerCase()}</h2>
-                    <p>The download is for {currentType.toLowerCase()} only.</p>
+                    <p style={{marginTop:'20px'}}>Are you sure you want to download. ? </p>
                     <div className={styles.modalActions}>
-                      <button onClick={handleDownload} className={styles.confirmButton}>Download</button>
-                      <button onClick={closeDownloadConfirm} className={styles.cancelButton}>Cancel</button>
+                      <button onClick={handleDownload} className={styles.confirmButton}>YES</button>
+                      <button onClick={closeDownloadConfirm} className={styles.cancelButton}>NO</button>
                     </div>
                   </div>
                 </div>
@@ -146,59 +162,56 @@ const HelpSection = () => {
         />
       }
 
-      <div className="container p-0">
+      <div className="container-fluid">
         <div className="row p-0 m-0 d-flex flex-column justify-content-around align-items-center col-12">
-          <div className="row d-flex flex-column justify-content-around align-items-center lg:min-h-[300px] xl:min-h-[400px]">
-            <div className={`d-flex p-3 ${styles.tableBoundary} mb-5 mt-5`}>
-            {guides.length ? (
+          <div className="row d-flex flex-column justify-content-around align-items-center">
+          <h1 className={styles.TOPName}>Help Center</h1>
+            <div className={`d-flex p-3 ${styles.tableBoundary} mb-5 mt-3`}>
+              {guides.length ? (
                 <>
-              <div
-                style={{ maxHeight: "73vh", minHeight: "40vh", overflow: "auto", width: '100%' }}
-              >
-                <table id="productGuidesTable" className="table table-responsive" style={{ minHeight: "150px" }}>
-                  <thead>
-                    <tr>
-                      <th className={`${styles.month} ${styles.stickyFirstColumnHeading}`} style={{ minWidth: "150px" }}>
-                        Name
-                      </th>
-                      <th className={`${styles.month} ${styles.stickyFirstColumnHeading}`} style={{ minWidth: "150px" }}>
-                        File Name
-                      </th>
-                      <th className={`${styles.month} ${styles.stickyFirstColumnHeading}`} style={{ minWidth: "150px" }}>
-                        Type
-                      </th>
-                      <th className={`${styles.month} ${styles.stickyFirstColumnHeading}`} style={{ minWidth: "150px" }}>
-                        Download link
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {filteredGuides.map((guide, index) => (
-                      <tr key={index}>
-                        <td className={styles.td}>
-                          {guide.Categoryname}
-                        </td>
-                        <td className={styles.td}>
-                          {guide.filename}
-                        </td>
-                        <td className={styles.td}>
-                          {guide.Type}
-                        </td>
-                        <td className={styles.td}>
-                          <button
-                            className={styles.btn}
-                            onClick={() => openModal(guide.Link, guide.Type)}
-                          >
-                            {guide.OriginalFileName}
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-              </>
-               ) : (
+             
+                  <div style={{ maxHeight: "73vh", minHeight: "40vh", overflow: "auto", width: '100%',  }}>
+                    
+                    <table id="productGuidesTable" className="table table-responsive" style={{ minHeight: "150px" , width:'100%'}}>
+                      <thead>
+                        <tr>
+                          <th className={`${styles.month} ${styles.stickyFirstColumnHeading}`} style={{ minWidth: "150px" }}>
+                          Category Name
+                          </th>
+                          <th className={`${styles.month} ${styles.stickyFirstColumnHeading}`} style={{ minWidth: "150px" }}>
+                            File Name
+                          </th>
+                          <th className={`${styles.month} ${styles.stickyFirstColumnHeading}`} style={{ minWidth: "150px" }}>
+                        Show View
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {filteredGuides.map((guide, index) => (
+                          <tr key={index}>
+                            <td className={styles.td}>
+                              {guide.Categoryname}
+                            </td>
+                            <td className={styles.td}>
+                              {guide.filename}
+                            </td>
+                            <td className={styles.td}>
+                              <button
+                                className={styles.btn}
+                                onClick={() => openModal(guide.Link, guide.Type, guide.filename)}
+                              >
+                          <div className="d-flex align-items-center justify-content-between gap-1" >
+                                <MdSlideshow  size={16}/> Show View
+                       </div>
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </>
+              ) : (
                 <Loading height={"70vh"} />
               )}
             </div>
@@ -210,3 +223,4 @@ const HelpSection = () => {
 };
 
 export default HelpSection;
+
