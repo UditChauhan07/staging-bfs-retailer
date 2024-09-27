cbdb4d9dd42af1c0f0e9575fbc57e5a41da75fbd
sangeetadx <sangeetadhiman.dx@gmail.com>
2024-09-27 18:00:20 +0530

    27.09.24.04

--- src/components/My Retailers/SelectBrandModel/SelectBrandModel.js
+++ src/components/My Retailers/SelectBrandModel/SelectBrandModel.js
@@ -30,8 +30,8 @@ const SelectBrandModel = ({ brands, onClose }) => {
                     // checked={selectedBrandAccountId === brand.AccountId__c}
                     onChange={() => {
                       // setSelectedBrandAccountId(brand.AccountId__c);
-                      localStorage.setItem("manufacturer", brand.ManufacturerName__c|| brand.Name);
-                      localStorage.setItem("ManufacturerId__c", brand.ManufacturerId__c||  brand.Id);
+                      localStorage.setItem("manufacturer", brand.ManufacturerName__c?? brand.Name);
+                      localStorage.setItem("ManufacturerId__c", brand.ManufacturerId__c??  brand.Id);
                       localStorage.setItem("Sales_Rep__c", brand.Sales_Rep__c);
                       localStorage.setItem("shippingMethod", JSON.stringify({number:brand.Shipping_Account_Number__c,method:brand.Shipping_Method__c}));
                       // if (selectedBrandManufacturer) {
