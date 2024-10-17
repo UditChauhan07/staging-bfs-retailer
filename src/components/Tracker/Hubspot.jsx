7e4719a6f63a17076cedbbc2bbd345638d2a1ad4
sangeetadx <sangeetadhiman.dx@gmail.com>
2024-10-17 11:35:10 +0530

    17.10.24.01

--- src/components/Tracker/Hubspot.jsx
+++ src/components/Tracker/Hubspot.jsx
@@ -1,40 +1,16 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const HubSpotTracker = () => {
     const location = useLocation();
 
     useEffect(() => {
-        const hubspotId = process.env.REACT_APP_HUBSPOTID;
-        console.log({hubspotId});
-        
-
-        if (!hubspotId) {
-            console.error('HubSpot ID is not defined in the environment variables.');
-            return;
+        if (window.hj) {
+          window.hj('stateChange', location.pathname);
         }
-
-        // Load HubSpot script dynamically
-        const script = document.createElement('script');
-        script.src = `//js.hs-scripts.com/${hubspotId}.js`;
-        script.async = true;
-
-        script.onload = () => {
-            if (window.hbspt) {
-                window.hbspt.analytics.trackPage(location.pathname);
-            }
-        };
-
-        document.body.appendChild(script);
-
-        // Track page views on location change
-        window.hbspt && window.hbspt.analytics.trackPage(location.pathname);
-
-        return () => {
-            // Clean up script on unmount
-            document.body.removeChild(script);
-        };
-    }, [location]);
+        console.log({aa:location.pathname});
+        
+      }, [location]);
 
     return null; // This component doesn't render anything
 };
