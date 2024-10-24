13c6f35678fbda8c375c2542581532bab26f0c18
sangeetadx <sangeetadhiman.dx@gmail.com>
2024-10-24 16:28:10 +0530

    23.10.24.12

--- src/components/loginUI/LoginUI.jsx
+++ src/components/loginUI/LoginUI.jsx
@@ -32,6 +32,9 @@ const LoginUI = () => {
         localStorage.setItem("emailB2B", values.email);
         localStorage.setItem("passwordB2B", values.password);
       }
+      window.hj('identify', apiData?.data?.retailerId, {
+        userName: apiData?.data?.Name,
+      });
       localStorage.setItem("Name", apiData?.data?.Name);
 
       localStorage.setItem("Api Data", JSON.stringify(apiData));
